212ec11af60904573c6f4f7891517ce1f45a6f78
skw0270 <70538441+skw0270@users.noreply.github.com>
2020-09-15 21:13:46 -0500

    Add files via upload

--- age.js
+++ age.js
@@ -1,3 +1,5 @@
-var floatAge=parseFloat(prompt("What is your age in years?")); floatDays=parseFloat(floatAge*365.25).toFixed(2); floatMonths=parseFloat(floatAge*12).toFixed(2); intWeeks=parseInt(floatDays/7).toFixed(2); intFortnights =parseInt(floatDays/14).toFixed(2);
+var floatAge=parseFloat(prompt("What is your age in years?")) ; floatDays=parseFloat(floatAge*365.25).toFixed(2); floatMonths=parseFloat(floatAge*12).toFixed(2); intWeeks=parseInt(floatDays/7).toFixed(2); intFortnights =parseInt(floatDays/14).toFixed(2);
 alert("Your age in days is " + floatDays + ", your age in months is " + floatMonths + ", your age in weeks is " + intWeeks + ", and your age in fortnights is " + intFortnights +".");
- 
+if(isNaN(floatAge)) {
+    alert("You have entered an invalid response, and an error has occured. Please Enter a Number.");
+}
